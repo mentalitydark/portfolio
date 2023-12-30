963166dd38fa12b5e11fa912d2f4d906a372cbb7
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2023-12-30 15:36:24 -0300

    feat: add config base para adicionar novas rotas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import 'dotenv/config';
 
 import http from 'http'
 import express from 'express'
+import { fileURLToPath } from "url";
+import path from 'path';
+
+import router from './router/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 const server = http.createServer(app)
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 server.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}.`)
 })
+
+app.use('/', router())
