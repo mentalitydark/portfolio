c6236feed7f4747b6d34acd1ddde7647ae1bdc7d
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-01-02 23:26:47 -0300

    feat: add nova rota projects

--- src/router/pages.js
+++ src/router/pages.js
@@ -6,4 +6,5 @@ import { staticFile } from '../utils/index.js'
 export default (router) => {
   router.get('/', (_, res) => res.sendFile(staticFile("page", "home.html")))
   router.get('/about-me', (_, res) => res.sendFile(staticFile("page", "about-me.html")))
+  router.get('/projects', (_, res) => res.sendFile(staticFile("page", "projects.html")))
 }
