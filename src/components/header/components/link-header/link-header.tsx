c0f6498cc657d62a875e519c9620539dd7138b11
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-02-14 08:47:20 -0300

    feat: adiciona uso de Link do navigation

--- src/components/header/components/link-header/link-header.tsx
+++ src/components/header/components/link-header/link-header.tsx
@@ -1,12 +1,15 @@
-import Link from 'next/link'
+'use client'
 
-import type { LinkProps } from './link-header-types'
+import type { LinkHeaderProps } from './link-header-types'
 
-export const LinkHeader = ({ text, href, selected }: LinkProps) => {
+import { Link } from '@/src/navigation'
 
+export const LinkHeader = ({ text, selected, href }: LinkHeaderProps) => {
   const color = selected ? 'text-green-500 dark:text-green-500' : 'text-slate-900 dark:text-slate-300'
 
   return (
-    <Link href={href} className={`${color} font-semibold mx-4 hover:font-bold`}>{text}</Link>
+    <Link href={{ pathname: href }} replace className={`${color} font-semibold mx-4 hover:font-bold`}>
+      {text}
+    </Link>
   )
 }
