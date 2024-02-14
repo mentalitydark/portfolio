2b5c5dcd4439f8ffc198df09af2b886b5eb4c137
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-02-14 08:45:50 -0300

    feat: add tradução dos textos

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,25 @@
+import { useTranslations } from 'next-intl'
+
 import type { HeaderProps } from './header-types'
 
 import { Pages } from '@/src/enums'
 
-import { DarkModeSwitch, LinkHeader } from './components'
+import { LanguageSelect, LinkHeader, DarkModeSwitch } from './components'
 
 export const Header = ({ currentPage }: HeaderProps) => {
+  const translate = useTranslations('components.header')
+
   return (
-    <header className='flex justify-end'>
-      <LinkHeader href='/' text='Página inicial' selected={currentPage === Pages.HOME} />
-      <LinkHeader href='/about-me' text='Sobre mim' selected={currentPage === Pages.ABOUT_ME} />
-      <LinkHeader href='/projects' text='Projetos' selected={currentPage === Pages.PROJECTS} />
-      <DarkModeSwitch />
+    <header className='flex justify-end max-[375px]:flex-col max-[375px]:items-center max-[375px]:w-full min-[375px]:pr-4'>
+      <div className='flex'>
+        <LinkHeader href='/' text={translate('home')} selected={currentPage === Pages.HOME} />
+        <LinkHeader href='/about-me' text={translate('about-me')} selected={currentPage === Pages.ABOUT_ME} />
+        <LinkHeader href='/projects' text={translate('projects')} selected={currentPage === Pages.PROJECTS} />
+      </div>
+      <div className='flex items-center max-[375px]:justify-evenly max-[375px]:w-full max-[375px]:mt-4'>
+        <DarkModeSwitch />
+        <LanguageSelect />
+      </div>
     </header>
   )
 }
